f6ffdee43724374a393be5588334f6aba5fadc3e
Josh Erkan <erkman.josh@gmail.com>
2021-03-30 18:58:02 -0400

    create scripture and get scripture

---
+++ SAS.Services/ScriptureService.cs
@@ -0,0 +1,64 @@
+﻿using SAS.Data;
+using SAS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SAS.Services
+{
+    class ScriptureService
+    {
+        private readonly Guid _userId;
+
+        public ScriptureService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateScripture(ScriptureCreate model)
+        {
+            var entity =
+                new Scripture()
+                {
+                    OwnerId = _userId,
+                    Book = model.Book,
+                    Chapter = model.Chapter,
+                    Verses = model.Verses,
+                    Content = model.Content,
+                    CreatedUTC = DateTimeOffset.Now
+                };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Scriptures.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<ScriptureListItem> GetScriptures()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Scriptures
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new ScriptureListItem
+                                {
+                                    ScriptureId = e.ScriptureId,
+                                    Book = e.Book,
+                                    Chapter = e.Chapter,
+                                    Verses = e.Verses,
+                                    CreatedUTC = e.CreatedUTC
+                                }
+                         );
+
+                return query.ToArray();
+            }
+        }
+    }
+}
